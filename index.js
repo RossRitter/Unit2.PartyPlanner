d5d762c6174d0df71689a13903d0499cd07644da
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-10-30 21:15:46 -0400

    did not finish but I think everything looks good but not sure

---
+++ index.js
@@ -0,0 +1,25 @@
+const API_URL =https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/events
+const state = {
+  events: []
+}
+
+const eventsList = document.querySelector('#events')
+const addEventForm = document.querySelector('#addEvent');
+addEventForm.addEventListener("submit", addEvent);
+
+async function render() {
+  await getEvents();
+  renderEventsDOM();
+}
+
+render();
+
+async function getEvents() {
+  try {
+      const res = await fetch(API_URL);
+      const json = await res.json();
+      state.events = json.data;
+  } catch (error) {
+      console.log(error);
+  }
+}
